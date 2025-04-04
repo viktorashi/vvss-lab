ec00141fbdef6e5d7afba11ef9ab67f981c5bb3b
viktorashi <ioanvictorstan@gmail.com>
2025-04-04 15:19:48 +0300

    asa da se se vada mai bine in teste

--- src/main/java/repository/AbstractCRUDRepository.java
+++ src/main/java/repository/AbstractCRUDRepository.java
@@ -37,7 +37,7 @@ public abstract class AbstractCRUDRepository<ID, E extends HasID<ID>> implements
             return Objects.requireNonNullElse(Previous, entity);
         }
         catch (ValidationException ve) {
-            System.out.println("Entitatea nu este valida! \n");
+            System.out.println(ve.getMessage());
             return null;
         }
     }
--- src/main/java/validation/AssignmentValidator.java
+++ src/main/java/validation/AssignmentValidator.java
@@ -5,19 +5,19 @@ import domain.Assignment;
 public class AssignmentValidator implements Validator<Assignment> {
     public void validate(Assignment assignment) throws ValidationException {
         if (assignment.getID() == null || assignment.getID().equals("")) {
-            throw new ValidationException("ID invalid! \n");
+            throw new ValidationException("ID invalid! nu poate fi null sau gol '' ");
         }
         if (assignment.getDescriere() == null || assignment.getDescriere().equals("")) {
-            throw new ValidationException("Descriere invalida! \n");
+            throw new ValidationException("Descriere invalida! nu poate fi nul sau gol ''");
         }
         if (assignment.getStartline() < 1 || assignment.getStartline() > 14) {
-            throw new ValidationException("Data de primire invalida! trebuie intre [1-14] \n");
+            throw new ValidationException("Data de primire invalida! trebuie intre [1-14] ");
         }
         if (assignment.getDeadline() < 1 || assignment.getDeadline() > 14) {
-            throw new ValidationException("Deadline invalid!  trebuie intre [1-14] \n");
+            throw new ValidationException("Deadline invalid!  trebuie intre [1-14] ");
         }
         if (assignment.getStartline() > assignment.getDeadline()) {
-            throw new ValidationException("Deadlineu inainte de startline!! \n");
+            throw new ValidationException("Deadlineu inainte de startline!! ");
         }
     }
 }
